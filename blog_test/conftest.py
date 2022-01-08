995473e75b3b5cf66e26130d9136e2a36e61e0d0
tylerpitcher <tylerpitch2015@gmail.com>
2022-01-08 14:42:11 -0500

    Add backend tests

--- blog_test/conftest.py
+++ blog_test/conftest.py
@@ -3,6 +3,11 @@ This file defines what to do BEFORE & AFTER running any test cases:
 '''
 
 import os
+import pytest
+import time
+import threading
+from werkzeug.serving import make_server
+from blog.__main__ import app
 
 db_file = os.getcwd() + '/db.sqlite'
 
@@ -23,3 +28,35 @@ def pytest_sessionfinish():
     print('Cleaning up environment...')
     if os.path.exists(db_file):
         os.remove(db_file)
+
+
+base_url = 'http://127.0.0.1:{}'.format(8081)
+
+
+class ServerThread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        # import necessary routes
+        self.srv = make_server('127.0.0.1', 8081, app)
+        self.ctx = app.app_context()
+        self.ctx.push()
+
+    def run(self):
+        print('running')
+        self.srv.serve_forever()
+
+    def shutdown(self):
+        self.srv.shutdown()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def server():
+    # create a live server for testing
+    # with a temporary file as database
+    server = ServerThread()
+    server.start()
+    time.sleep(5)
+    yield
+    server.shutdown()
+    time.sleep(2)
---
+++ blog_test/test_frontend/test_front_comment.py
@@ -0,0 +1,36 @@
+from seleniumbase import BaseCase
+from blog_test.conftest import base_url
+
+from blog.models import register, post
+
+
+class FrontEndHomePageTest(BaseCase):
+
+    def test_comment_success(self, *_):
+        u = register('frontComment', 'frontComment@test.ca', '#1abc', '#1abc')
+        post(u, 'Comment Here', '')
+
+        self.open(base_url + '/login')
+        self.type('#email', 'frontComment@test.ca')
+        self.type('#password', '#1abc')
+        self.click('input[type="submit"]')
+
+        self.open(base_url + '/profile')
+        self.click('.posts .snippet a')
+
+        self.type('input[type="text"]', 'my comment')
+        self.click('input[type="submit"]')
+
+        self.assert_element('.comments .content')
+
+    def test_comment_fail(self, *_):
+        u = register('noComment', 'noComment@test.ca', '#1abc', '#1abc')
+        post(u, 'No Comment', '')
+
+        self.open(base_url)
+        self.click('.posts .snippet')
+
+        self.type('input[type="text"]', 'my comment')
+        self.click('input[type="submit"]')
+
+        self.assert_element('.alert-error')
