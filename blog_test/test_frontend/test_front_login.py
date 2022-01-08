ed6f46aa94af4ed4730d6b3345f766b9968a5808
tylerpitcher <tylerpitch2015@gmail.com>
2022-01-08 17:43:43 -0500

    Modify frontend login tests

--- blog_test/test_frontend/test_front_login.py
+++ blog_test/test_frontend/test_front_login.py
@@ -1,27 +1,27 @@
 from seleniumbase import BaseCase
 from blog_test.conftest import base_url
 
-# from blog.models import register
+import time
 
 
 class FrontEndHomePageTest(BaseCase):
 
     def test_login_success(self, *_):
-        # register('frontLogin', 'frontLogin@test.ca', '#1abc', '#1abc')
+        self.open(base_url + '/signup')
+        self.type('#email', 'frontLogin@test.ca')
+        self.type('#username', 'frontLogin')
+        self.type('#password1', '#1abc')
+        self.type('#password2', '#1abc')
+        self.click('input[type="submit"]')
+        
+        self.assert_element(".alert-success")
+
+    def test_login_fail(self, *_):
+        self.open(base_url + '/login')
 
         self.open(base_url + '/login')
-    #     self.type('#email', 'frontLogin@test.ca')
-    #     self.type('#password', '#1abc')
-    #     self.click('input[type="submit"]')
+        self.type('#email', 'noLogin@test.ca')
+        self.type('#password', '#1abc')
+        self.click('input[type="submit"]')
 
-    #     self.assert_element(".alert-success")
-
-    # def test_login_fail(self, *_):
-    #     self.open(base_url + '/login')
-
-    #     self.open(base_url + '/login')
-    #     self.type('#email', 'noLogin@test.ca')
-    #     self.type('#password', '#1abc')
-    #     self.click('input[type="submit"]')
-
-    #     self.assert_element(".alert-error")
+        self.assert_element(".alert-error")
