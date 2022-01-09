aa3b713acd8599c2dbc4f6882906ef9ee4fe5e82
tylerpitcher <tylerpitch2015@gmail.com>
2022-01-08 19:42:04 -0500

    Fix pep8 issues

--- blog_test/test_backend/test_comment.py
+++ blog_test/test_backend/test_comment.py
@@ -1,43 +1,43 @@
-# '''
-# Tests comment function defined in models.py.
-# '''
+'''
+Tests comment function defined in models.py.
+'''
 
-# from blog.models import User, Post, register, post, comment
-
-
-# def test_user_requirements():
-#     '''
-#     Tests that only real users can comment on posts.
-#     '''
-#     real_user = register('realUser2', 'realUser2@test.ca', '#1abc', '#1abc')
-#     fake_user = User(
-#         username='fakeUser',
-#         email='fakeUser@test.ca',
-#         password='#1abc'
-#     )
-#     user_post = post(real_user, 'comments are cool', '')
-
-#     assert comment(real_user, user_post, 'this is a cool post') is not None
-#     assert comment(fake_user, user_post, 'this is a cool post') is None
+from blog.models import User, Post, register, post, comment
 
 
-# def test_post_requirements():
-#     '''
-#     Tests that a user can only comment on a real post.
-#     '''
-#     user = register(
-#         'commentsOnPosts',
-#         'commentsOnPosts@test.ca',
-#         '#1abc',
-#         '#1abc'
-#     )
+def test_user_requirements():
+    '''
+    Tests that only real users can comment on posts.
+    '''
+    real_user = register('realUser2', 'realUser2@test.ca', '#1abc', '#1abc')
+    fake_user = User(
+        username='fakeUser',
+        email='fakeUser@test.ca',
+        password='#1abc'
+    )
+    user_post = post(real_user, 'comments are cool', '')
 
-#     real_post = post(user, 'a real post', '')
-#     fake_post = Post(
-#         username=user.username,
-#         title='A Real Post',
-#         content=''
-#     )
+    assert comment(real_user, user_post, 'this is a cool post') is not None
+    assert comment(fake_user, user_post, 'this is a cool post') is None
 
-#     assert comment(user, real_post, 'this was a real post') is not None
-#     assert comment(user, fake_post, 'this was a fake post') is None
+
+def test_post_requirements():
+    '''
+    Tests that a user can only comment on a real post.
+    '''
+    user = register(
+        'commentsOnPosts',
+        'commentsOnPosts@test.ca',
+        '#1abc',
+        '#1abc'
+    )
+
+    real_post = post(user, 'a real post', '')
+    fake_post = Post(
+        username=user.username,
+        title='A Real Post',
+        content=''
+    )
+
+    assert comment(user, real_post, 'this was a real post') is not None
+    assert comment(user, fake_post, 'this was a fake post') is None
--- blog_test/test_backend/test_register.py
+++ blog_test/test_backend/test_register.py
@@ -1,53 +1,53 @@
-# '''
-# Tests register function declared in models.py
-# '''
+'''
+Tests register function declared in models.py
+'''
 
-# from blog.models import register, User
-
-
-# def test_username_requirements():
-#     '''
-#     Tests that only valid usernames can be used to register.
-#     '''
-#     assert isinstance(
-#         register('UPPERCASE', 'uppercase@test.ca', '#1abc', '#1abc'), User)
-#     assert isinstance(
-#         register('lowercase', 'lowercase@test.ca', '#1abc', '#1abc'), User)
-#     assert isinstance(
-#         register('numbers123', 'numbers123@test.ca', '#1abc', '#1abc'), User)
-#     assert isinstance(register(
-#         'numbers1Mixed2In3', '1nums2@test.ca', '#1abc', '#1abc'), User)
-#     assert not isinstance(
-#         register('1numFirst', 'numFirst@test.ca', '#1abc', '#1abc'), User)
-#     assert not isinstance(
-#         register('tooLong' * 10, 'tooLong@test.ca', '#1abc', '#1abc'), User)
-#     assert not isinstance(
-#         register('slim', 'tooShort@test.ca', '#1abc', '#1abc'), User)
+from blog.models import register, User
 
 
-# def test_email_requirements():
-#     '''
-#     Tests that only valid rfc5322 emails can be used to register.
-#     '''
-#     assert isinstance(
-#         register('validEmail', 'valid.email@test.ca', '#1abc', '#1abc'), User)
-#     assert not isinstance(
-#         register('dot', '.dot@test.ca', '#1abc', '#1abc'), User)
-#     assert not isinstance(
-#         register('notAt', 'notAt.ca', '#1abc', '#1abc'), User)
+def test_username_requirements():
+    '''
+    Tests that only valid usernames can be used to register.
+    '''
+    assert isinstance(
+        register('UPPERCASE', 'uppercase@test.ca', '#1abc', '#1abc'), User)
+    assert isinstance(
+        register('lowercase', 'lowercase@test.ca', '#1abc', '#1abc'), User)
+    assert isinstance(
+        register('numbers123', 'numbers123@test.ca', '#1abc', '#1abc'), User)
+    assert isinstance(register(
+        'numbers1Mixed2In3', '1nums2@test.ca', '#1abc', '#1abc'), User)
+    assert not isinstance(
+        register('1numFirst', 'numFirst@test.ca', '#1abc', '#1abc'), User)
+    assert not isinstance(
+        register('tooLong' * 10, 'tooLong@test.ca', '#1abc', '#1abc'), User)
+    assert not isinstance(
+        register('slim', 'tooShort@test.ca', '#1abc', '#1abc'), User)
 
 
-# def test_password_requirements():
-#     '''
-#     Tests that only valid passwords can be used to register.
-#     '''
-#     assert isinstance(register(
-#         'validPassw', 'validPassw@test.ca', 'v#lid123', 'v#lid123'), User)
-#     assert not isinstance(
-#         register('longPwd', 'longPwd@test.ca', 'long' * 40, 'long' * 40), User)
-#     assert not isinstance(
-#         register('shortPassword', 'shortPassword@test.ca', '#1a', '#1a'), User)
-#     assert not isinstance(
-#         register('noSpeical', 'noSepcial@test.ca', '123ab', '123ab'), User)
-#     assert not isinstance(
-#         register('noNums', 'noNums@test.ca', '#abcd', '#abcd'), User)
+def test_email_requirements():
+    '''
+    Tests that only valid rfc5322 emails can be used to register.
+    '''
+    assert isinstance(
+        register('validEmail', 'valid.email@test.ca', '#1abc', '#1abc'), User)
+    assert not isinstance(
+        register('dot', '.dot@test.ca', '#1abc', '#1abc'), User)
+    assert not isinstance(
+        register('notAt', 'notAt.ca', '#1abc', '#1abc'), User)
+
+
+def test_password_requirements():
+    '''
+    Tests that only valid passwords can be used to register.
+    '''
+    assert isinstance(register(
+        'validPassw', 'validPassw@test.ca', 'v#lid123', 'v#lid123'), User)
+    assert not isinstance(
+        register('longPwd', 'longPwd@test.ca', 'long' * 40, 'long' * 40), User)
+    assert not isinstance(
+        register('shortPassword', 'shortPassword@test.ca', '#1a', '#1a'), User)
+    assert not isinstance(
+        register('noSpeical', 'noSepcial@test.ca', '123ab', '123ab'), User)
+    assert not isinstance(
+        register('noNums', 'noNums@test.ca', '#abcd', '#abcd'), User)
